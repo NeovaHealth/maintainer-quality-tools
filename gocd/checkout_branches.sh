461701d7a765dd6b3ffcf041615c5466652ded18
Colin Wren <colin.wren@bjss.com>
2017-09-08 14:09:13 +0100

    - Adding script to handle checking out branches

---
+++ gocd/checkout_branches.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+BASE_BRANCH="develop"
+BRANCH=$BASE_BRANCH
+if [ $GO_SCM_NHCLINICAL_PR_ID != $BASE_BRANCH ] then;
+    BRANCH=$GO_SCM_NHCLINICAL_PR_ID
+if [ $GO_SCM_OPEN_EOBS_PR_ID != $BASE_BRANCH ] then;
+    BRANCH=$GO_SCM_OPEN_EOBS_PR_ID
+if [ $GO_SCM_CLIENT_MODULES_PR_ID != $BASE_BRANCH ] then;
+    BRANCH=$GO_SCM_CLIENT_MODULES_PR_ID
+cd ../nhclinical
+git fetch
+git checkout $BRANCH || true
+cd ../openeobs
+git fetch
+git checkout $BRANCH || true
+cd ../client_modules
+git fetch
+git checkout $BRANCH || true
+cd ..
