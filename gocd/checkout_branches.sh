3d3bf5220b4f91fd43c690dbbcfa7f225b209d0b
Colin Wren <colin.wren@bjss.com>
2017-09-08 14:14:53 +0100

    Need to go up another directory

--- gocd/checkout_branches.sh
+++ gocd/checkout_branches.sh
@@ -7,6 +7,7 @@ if [ $GO_SCM_OPEN_EOBS_PR_ID != $BASE_BRANCH ] then;
     BRANCH=$GO_SCM_OPEN_EOBS_PR_ID
 if [ $GO_SCM_CLIENT_MODULES_PR_ID != $BASE_BRANCH ] then;
     BRANCH=$GO_SCM_CLIENT_MODULES_PR_ID
+cd ..
 cd ../nhclinical
 git fetch
 git checkout $BRANCH || true
