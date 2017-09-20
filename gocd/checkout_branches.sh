bb7a1fae580902cb03e1052015d134f9d2d287b3
Colin Wren <colin.wren@bjss.com>
2017-09-20 16:01:20 +0100

    Using git commit's date as unixtimestamp to determine which branch should be on

--- gocd/checkout_branches.sh
+++ gocd/checkout_branches.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 BASE_BRANCH="develop"
 BRANCH=$BASE_BRANCH
+
 NHCLINICAL_BRANCH=$(echo $GO_SCM_NHCLINICAL_PR_BRANCH | sed 's/NeovaHealth://')
 OPENEOBS_BRANCH=$(echo $GO_SCM_OPEN_EOBS_PR_BRANCH | sed 's/NeovaHealth://')
 CLIENT_BRANCH=$(echo $GO_SCM_CLIENT_MODULES_PR_BRANCH | sed 's/bjss://')
-if [ "${NHCLINICAL_BRANCH}" != "${BASE_BRANCH}" ] ; then
+
+NHCLINICAL_HASH=$GO_SCM_NHCLINICAL_LABEL
+cd ../nhclinical
+NHCLINICAL_DATE=$(git show "${NHCLINICAL_HASH}" --format="%at")
+
+OPENEOBS_HASH=$GO_SCM_OPEN_EOBS_LABEL
+cd ../openeobs
+OPENEOBS_DATE=$(git show "${OPENEOBS_HASH}" --format="%at")
+
+CLIENT_HASH=$GO_SCM_CLIENT_MODULES_LABEL
+cd ../client_modules
+CLIENT_DATE=$(git show "${CLIENT_HASH}" --format="%at")
+
+# Need to then compare all 3 hashes
+if [ $NHCLINICAL_HASH -gt $OPENEOBS_HASH ] && [ $NHCLINICAL_HASH -gt $CLIENT_HASH ] ; then
     BRANCH=$NHCLINICAL_BRANCH
 fi
-if [ "${OPENEOBS_BRANCH}" != "${BASE_BRANCH}" ] ; then
+if [ $OPENEOBS_HASH -gt $NHCLINICAL_HASH ] && [ $OPENEOBS_HASH -gt $CLIENT_HASH ] ; then
     BRANCH=$OPENEOBS_BRANCH
 fi
-if [ "${CLIENT_BRANCH}" != "${BASE_BRANCH}" ] ; then
+if [ $CLIENT_HASH -gt $NHCLINICAL_HASH ] && [ $CLIENT_HASH -gt $OPENEOBS_HASH ] ; then
     BRANCH=$CLIENT_BRANCH
 fi
 cd ../nhclinical
